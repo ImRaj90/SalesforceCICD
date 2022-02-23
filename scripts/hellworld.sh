c1ad5515a53a8faaf7f789aa133749abafe2941c
Rajashekar Jangam <swamyrajashekar@gmail.com>
2022-02-24 00:55:43 +0530

    Update hellworld.sh

--- scripts/hellworld.sh
+++ scripts/hellworld.sh
@@ -4,5 +4,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh
 nvm install node
 node -e "console.log('Running Node.js ' + process.version)"
 echo "checking if npm installed"
+npm -v
+npm install --global sfdx-cli
 echo 'y' | sfdx plugins:install sfpowerkit
 echo 'y' | sfdx plugins:install @dxatscale/sfpowerscripts
