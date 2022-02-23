a38afc4474da880aa132bab7d8cd0944b330144a
Rajashekar Jangam <swamyrajashekar@gmail.com>
2022-02-24 00:31:23 +0530

    Update hellworld.sh

--- scripts/hellworld.sh
+++ scripts/hellworld.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "Hello World"
+wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz
+mkdir sfdx-cli
+tar xJf sfdx-linux-amd64.tar.xz -C sfdx-cli --strip-components 1
+./sfdx-cli/install
+echo 'y' | sfdx plugins:install sfpowerkit
+echo 'y' | sfdx plugins:install @dxatscale/sfpowerscripts
