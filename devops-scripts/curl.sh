823c15b8576ead3082b7a1f988b95d8060fcdb28
vicky <swamyrajashekar@gmail.com>
2023-04-12 22:18:14 +0530

    Update curl.sh

--- devops-scripts/curl.sh
+++ devops-scripts/curl.sh
@@ -2,6 +2,6 @@
 
 curl -L \
   -H "Accept: application/vnd.github+json" \
-  -H "Authorization: Bearer {{secrets.GITHUBTOKENCURL}}"\
+  -H "Authorization: Bearer $GITHUBTOKENCURL" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/ImRaj90/SalesforceCICD/actions/workflows
